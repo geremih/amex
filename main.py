34ddf0e80d251305f46ae393dea4fe2348a975ee
ppsreejith <mailz4sreejith@gmail.com>
2015-10-04 07:31:24 +0530

    Rank 3 ftw

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from sklearn import svm
 from  sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import RandomForestClassifier as Forest
+import xgboost as xgb
 
 def clean_helper(df):
     for column in ['p_voted','n_unique_p','d_centaur', 'd_ebony', 'd_tokugawa', 'd_odyssey', 'd_cosmos', 's_centaur', 's_ebony', 's_tokugawa', 's_cosmos', 's_odyssey',  'n_centaur', 'n_ebony', 'n_tokugawa', 'n_odyssey', 'n_rallies', 'n_cosmos', 'docs'] :
@@ -81,7 +82,7 @@ def clean_helper(df):
         
     df['region'] = df['region'].fillna(57)
     for i in range(1,58):
-        df[str(i)] = (df['region'] == i).astype(int)
+        df['r' + str(i)] = (df['region'] == i).astype(int)
     #we are not making unknown region a feature
     for column in ['d_centaur', 'd_ebony', 'd_tokugawa', 'd_odyssey', 'd_cosmos', 's_centaur', 's_ebony', 's_tokugawa', 's_cosmos', 's_odyssey',  'n_centaur', 'n_ebony', 'n_tokugawa', 'n_odyssey', 'n_rallies', 'n_cosmos'] :
         df[column] = df[column].fillna(0)
@@ -101,7 +102,7 @@ def clean_testing (df):
 
 
 def get_features(df):
-    regions = [str(i) for i in range(1,58)]
+    regions = ['r' + str(i) for i in range(1,58)]
     ages = ['a' + str(i) for i in range(0,5)]
     edus = ['e' + str(i) for i in range(0,4)]
     return  df[['p_centaur', 'p_ebony', 'p_tokugawa', 'p_odyssey', 'p_cosmos','d_centaur', 'd_ebony', 'd_tokugawa', 'd_odyssey', 'd_cosmos', 's_centaur', 's_ebony', 's_tokugawa', 's_cosmos', 's_odyssey',  'n_centaur', 'n_ebony', 'n_tokugawa', 'n_odyssey', 'n_rallies','n_cosmos','n_unique_p','income', 'r_years','n_unique_p', 'p_voted', 'politics'] + regions + ages + edus]
@@ -111,13 +112,15 @@ def get_features(df):
 def get_model():
 #    return LogisticRegression()
 #    return svm.SVC()
-    return GradientBoostingClassifier()
+#    return GradientBoostingClassifier(n_estimators=200, max_depth=4)
+    return xgb.XGBClassifier(max_depth = 4, n_estimators=200)
 
 
 def testing(model):
     tf = pd.read_csv('testing.csv')
     tf = clean_testing(tf)
     X = get_features(tf)
+    X = fix_data(X)
     t = model.predict(X)
     ids = tf['Citizen_ID']
     t = [categories[i] for i in t]
@@ -142,7 +145,19 @@ def metric(X,y):
     print 'cross_val_score = %s' %(scores)
     print 'cross val average ', np.mean(scores)
 
-
+def fix_data(X):
+    #duplicate n_unique_p
+    temp = X['n_unique_p']
+    del X['n_unique_p']
+    X['n_unique_p'] = temp.ix[:,0]
+    
+    #making columns strictly alphanumeric
+    oldcols = X.columns
+    newcols = []
+    for col in oldcols:
+        newcols.append(col.replace('_',''))
+    X.columns = newcols
+    return X
 
 df = pd.read_csv('training.csv')
 df = clean_training(df)
@@ -150,7 +165,7 @@ categories = df['actual_vote'].cat.categories
 df['actual_vote'] = df['actual_vote'].cat.rename_categories(np.arange(5))
 
 
-y = np.ravel(df['actual_vote'])
+y = df['actual_vote']
 
 
 X = get_features(df)
@@ -160,10 +175,11 @@ print X.columns
 print 'X.shape = %s  y.shape = %s' %(X.shape,y.shape)
 #model = LogisticRegression()
 model = get_model()
+
+X = fix_data(X)
+
 model = model.fit(X, y)
 print model.score(X, y)
-
-
 
 #metric(X,y)
 testing(model)
